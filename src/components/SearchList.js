ca5a1463c5868a789638730bd4d33b23099d283c
ken.hurtado <khurtado@protonmail.com>
2018-07-16 16:15:00 -0600

    Branch test.

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
+import '../styles/index.css';
+
 function SearchList(props) {
-    let { strAlbum, name, type } = props;
-    console.log('props', props);
-    if (type === 'album') {
-        return (
-            <div>
-                <p>Album Name: {strAlbum ? strAlbum : name}</p>
-            </div>
-        );
-    }
-    if (type === 'track') {
-        return (
-            <div>
-                <p>Track Name: {name}</p>
-            </div>
-        );
-    }
+	let {
+		strAlbum, name, artist, type 
+	} = props;
+
+	if (type === 'album') {
+		return (
+			<div className="search-item">
+				<p className="search-txt">Album Name: {strAlbum ? strAlbum : name}</p>
+			</div>
+		);
+	}
+	if (type === 'track') {
+		return (
+			<div className="search-item">
+				<h4 className="search-txt">Track Name: {name}</h4>
+				<p className="search-artist-txt">Artist Name: {artist}</p>
+			</div>
+		);
+	}
 }
 
 export default SearchList;
--- src/views/Search.js
+++ src/views/Search.js
@@ -7,6 +7,9 @@ class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            audioDbAlbumsLimit: 5,
+            lastFmAlbumsLimit: 5,
+            lastFmTracksLimit: 5,
             bioResults: '',
             userInput: '',
             lastFmArtistImg: '',
@@ -19,137 +22,159 @@ class Search extends Component {
     }
 
     getRequest = (e) => {
-        let { userInput } = this.state;
-        e.preventDefault();
-        // Search results from query by artist name, bio and artist image returned
+        let { userInput } = this.state; // Destruct state
+        e.preventDefault(); // Prevent refresh
+
+        /**
+         * GET request to lastFM API:
+         * Query by artist name returns artist bio and artist image
+         */
         axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=5d6a4c2be0bcb377567ac2c3edd9f472&artist=${userInput}&format=json`)
-            .then((response) => {
+            .then((res) => {
                 this.setState({
-                    bioResults: response.data.artist.bio.content,
-                    lastFmArtistImg: response.data.artist.image[4]['#text']
+                    bioResults: res.data.artist.bio.content, // Set string state to artist bio from response
+                    lastFmArtistImg: res.data.artist.image[4]['#text'] // Set state to artist image from response
                 });
-                this.clearInputs();
+                this.clearInputs(); // clear input fields
             })
             .catch((err) => console.error(err));
 
-        // AudioDB search results from query by artist name, album name and album image returned
+        /**
+         * GET request to theaudioDB API:
+         * Query by artist name returns artist's album names
+         */
         axios.get(`http://www.theaudiodb.com/api/v1/json/195003/searchalbum.php?s=${userInput}`)
             .then((res) => {
-                let audioDbAlbumsRes = res.data.album;
+                let audioDbAlbumsRes = res.data.album; // Declare variable and set to response of array of album objects
                 this.data = res.data.album;
-                this.data.forEach((item) => {
-                    // console.log('Found: Item1', item);
-                    item.type = 'album';
-                    console.log('Found Album1: ', item.strAlbum);
-                    // console.log('Found Thumb1: ', item.strAlbumThumb);
+                this.data.forEach((item) => { // For each album object item
+                    item.type = 'album'; // add property named type and set value to album
                 });
-                // this.data.map((Album1, index) => {
-                //     console.log('Search1 Albums: ', Album1.strAlbum);
-                // });
                 this.setState({
-                    audioDbAlbums: audioDbAlbumsRes,
+                    audioDbAlbums: audioDbAlbumsRes // Set array state to array of album objects from response
                 });
-                this.clearInputs();
+                this.clearInputs(); // clear input fields
             })
             .catch((err) => console.error('Err1: ', err));
 
-        // LastFM search results from query by artist name, album name returned
+        /**
+         * GET request to lastFM API:
+         * Query by artist name returns artist's album names
+         */
         axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${userInput}&api_key=5d6a4c2be0bcb377567ac2c3edd9f472&format=json`)
             .then((res) => {
-                let lastFmAlbumsRes = res.data.topalbums.album;
-                this.data = res.data.topalbums.album;
-                this.data.forEach((item) => {
-                    item.type = 'album';
-                    console.log('Found Item2: ', item);
-                    console.log('Found Image2: ', item.image[3]['#text']);
-                    console.log('Found Album2: ', item.name);
+                let lastFmAlbumsRes = res.data.topalbums.album; // Declare variable and set to response of array of album objects
+                this.data = res.data.topalbums.album; // Set this instance of data to array of album objects from response
+                this.data.forEach((item) => { // For each album object item
+                    item.type = 'album'; // add property named type and set value to album
                 });
-                // this.data.map((Album2, index) => {
-                //     console.log('Search2 Albums: ', Album2.name);
-                // });
                 this.setState({
-                    lastFmAlbums: lastFmAlbumsRes
+                    lastFmAlbums: lastFmAlbumsRes // Set array state to array of album objects from response
                 });
-                this.clearInputs();
+                this.clearInputs(); // clear input fields
             })
             .catch((err) => console.error('Err2: ', err));
 
-        // Search results from query by album name, returns album name and album image
-        axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${userInput}&api_key=5d6a4c2be0bcb377567ac2c3edd9f472&format=json`)
-            .then((res) => {
-                this.data = res.data.albummatches.album;
-                this.data.forEach((item) => {
-                    item.type = 'album';
-                    console.log('Found Item3: ', item);
-                    console.log('Found Image3: ', item.image[3]['#text']);
-                    console.log('Found Album3: ', item.name);
-                });
-                this.clearInputs();
-            })
-            .catch((err) => console.error('Err3: ', err));
         
-        // Search results by artist/track name, returns artist/track name
+        /**
+         * GET request to LastFM API:
+         * Query by track name returns artist name and album name
+         */
         axios.get(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${userInput}&limit=10&api_key=5d6a4c2be0bcb377567ac2c3edd9f472&format=json`)
             .then((res) => {
-                let lastFmTracksRes = res.data.results.trackmatches.track;
-                this.data = res.data.results.trackmatches.track;
-                this.data.forEach((item) => {
-                    item.type = 'track';
-                    console.log('Found Item5: ', item);
-                    console.log('Found Item5 Track: ', item.track);
-                    console.log('Found Item5 Artist: ', item.artist);
-                });
-                this.data.map((track, index) => {
-                    console.log('Search5 Tracks: ', track.name);
-                    console.log('Search5 Track URL: ', track.url);
+                let lastFmTracksRes = res.data.results.trackmatches.track; // Declare variable and set to response of array of track objects
+                this.data = res.data.results.trackmatches.track; // Set this instance of data to array of track objects from response
+                this.data.forEach((item) => { // For each track object item
+                    item.type = 'track'; // add property named type and set value to track
                 });
                 this.setState({
-                    lastFmTracks: lastFmTracksRes
+                    lastFmTracks: lastFmTracksRes // Set array state to array of track objects from response
                 });
-                this.clearInputs();
+                this.clearInputs(); // clear input fields
             })
             .catch((err) => console.error('Err5: ', err));
+
+        /**
+         * GET request to SongKick
+         * Query by artist name returns artist ID
+         * which will be needed to search for artist's upcoming event
+         */
+        axios.get(`https://api.songkick.com/api/3.0/search/artists.json?apikey=EasSil2s2rpezUZr&query=${userInput}&page=1`)
+            .then((res) => {
+                console.log('SongKick Artist ID :', res.data.resultsPage.results.artist[0].id);
+                console.log('SongKick On Tour Status :', res.data.resultsPage.results.artist[0].onTourUntil);
+            })
+            .catch((err) => console.error('Err3: ', err));
     };
 
+    /**
+     * Method to clear user input from input field
+     */
     clearInputs = () => {
-        this.setState({
-            userInput: ''
+        this.setState({ // Set state on this instance
+            userInput: '' // of variable to empty string
         });
     };
 
+    /**
+     * Method to clear previous state of userInput
+     * and set to current target of the value of
+     * userInput
+     */
     handleChange = (e) => {
-        let { value } = e.target;
-        this.setState((prevState) => {
-            return { userInput: value };
+        let { value } = e.target; // Set variable to current value of event target
+        this.setState(() => {
+            /**
+             * Clear previous state of arrays, then state of userInput to current value, return value of userInput for GET request
+             */
+            return {
+                audioDbAlbumsLimit: 5, lastFmAlbumsLimit: 5, lastFmTracksLimit: 5, audioDbAlbums: [], lastFmAlbums: [], lastFmTracks: [], lastFmArtist: [], userInput: value
+            };
         });
     };
 
     render() {
-        let { userInput, bioResults, lastFmArtistImg, audioDbAlbums, lastFmAlbums, lastFmTracks } = this.state;
+        let {
+            userInput, bioResults, lastFmArtistImg, audioDbAlbums, lastFmAlbums, lastFmTracks, audioDbAlbumsLimit, lastFmAlbumsLimit, lastFmTracksLimit
+        } = this.state; // Destruct state
         return (
             <div className="component-wrapper">
                 <h2>Start Your Search For Some Tasty Tunes</h2>
+                {/* Form for submitting query */}
+                {/* On form submit call getRequest method to initiate GET requests to APIs */}
                 <form onSubmit={this.getRequest}>
                     <fieldset>
                         <label>
+                            {/* On change of form input call handleChange method to set the current state of the event target value userInput */}
                             <input onChange={this.handleChange} value={userInput} name="artist" type="text" />
                             <button type="submit">Search</button>
                         </label>
                     </fieldset>
                 </form>
                 <div>
-                    <img src={lastFmArtistImg} alt="" />
-                    <p>{bioResults}</p>
+                    <img className="bio-img" src={lastFmArtistImg} alt="" /> {/* Render artist image from API GET request */}
+                    <p className="bio-txt">{bioResults}</p> {/* Render artist bio from API GET request */}
                     <div>
-                        {audioDbAlbums.map((audioDbAlbum, i) => {
+                        {/* Response from theaudiodb GET request: Map through each album object one at a time and then return results to SearchList  */}
+                        {audioDbAlbums.slice(0, audioDbAlbumsLimit).map((audioDbAlbum, i) => {
                             return <SearchList key={i} {...audioDbAlbum}></SearchList>;
                         })}
                     </div>
+                    <div className="more-albums-button" onClick={() => this.setState({ audioDbAlbumsLimit: this.state.audioDbAlbumsLimit + 5 })}><h4>Click here to load 5 more albums from TheAudioDB...</h4></div>
                     <div>
-                        {lastFmAlbums.map((lastFmAlbum, i) => {
+                    { /* Response from LastFM GET request: Map through each album object one at a time and then return results to SearchList  */}
+                        {lastFmAlbums.slice(0, lastFmAlbumsLimit).map((lastFmAlbum, i) => {
                             return <SearchList key={i} {...lastFmAlbum}></SearchList>;
                         })}
                     </div>
+                    <div className="more-albums-button" onClick={() => this.setState({ lastFmAlbumsLimit: this.state.lastFmAlbumsLimit + 5 })}><h4>Click here to load 5 more albums from LastFM...</h4></div>
+                    <div>
+                    { /* Response from LastFM GET request: Map through each track object one at a time and then return results to SearchList  */}
+                        {lastFmTracks.slice(0, lastFmTracksLimit).map((lastFmTrack, i) => {
+                            return <SearchList key={i} {...lastFmTrack}></SearchList>;
+                        })}
+                    </div>
+                    <div className="more-tracks-button" onClick={() => this.setState({ lastFmTracksLimit: this.state.lastFmTracksLimit + 5 })}><h4>Click here to load 5 more tracks from LastFM...</h4></div>
                 </div>
             </div>
         );
